59b4237feee05d2ab5ee233cffad7c0638bcf6a1
Jafeht Soturno <jasordev@gmail.com>
2021-04-26 15:20:59 -0400

    implementadno las operaciones de buyer controller

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Buyer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Buyer;
 use Illuminate\Http\Request;
 
 class BuyerController extends Controller
@@ -14,7 +15,9 @@ class BuyerController extends Controller
      */
     public function index()
     {
-        //
+        $buyers = Buyer::has('transactions')->get();
+
+        return response()->json(['data' => $buyers], 200);
     }
 
     /**
@@ -46,7 +49,7 @@ class BuyerController extends Controller
      */
     public function show($id)
     {
-        //
+
     }
 
     /**
@@ -80,6 +83,8 @@ class BuyerController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $buyer = Buyer::has('transactions')->findOrFail($id);
+
+        return response()->json(['data' => $buyer], 200);
     }
 }
