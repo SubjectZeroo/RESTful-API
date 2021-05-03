05435e98d787f2b330336e28ad46225d132328b8
Jafeht Soturno <jasordev@gmail.com>
2021-05-03 11:42:02 -0400

    caching resultados con el sistema de laravel

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -4,6 +4,7 @@ namespace App\Traits;
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Validator;
 
 trait ApiResponser
@@ -33,6 +34,7 @@ trait ApiResponser
 
         $collection = $this->transformData($collection, $transformer);
 
+        $collection = $this->cacheResponse($collection);
 
         return $this->successResponse($collection, $code);
 
@@ -113,5 +115,23 @@ trait ApiResponser
 
         return $transformation->toArray();
 
+    }
+
+
+    protected function cacheResponse($data)
+    {
+        $url = request()->url();
+
+        $queryParams = request()->query();
+
+        ksort($queryParams);
+
+        $queryString = http_build_query($queryParams);
+
+        $fullUrl = "{$url}?{$queryString}";
+
+        return Cache::remember($fullUrl, 30/60, function() use($data) {
+            return $data;
+        });
     }
 }
